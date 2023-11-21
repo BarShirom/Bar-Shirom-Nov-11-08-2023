84baa9028fa473d5a6533528d8b8d2c3715a5fa1
BarShirom <shirombar@gmail.com>
2023-11-21 12:34:47 +0200

    second commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import "./Header.css"
-
+import "./Header.css";
 
 const Header = () => {
   return (
     <>
       <nav className="navbar">
-        <h1>Movies app</h1>
-        <Link className="link" to="/home">Home</Link>
-        <Link className="link" to="/favorites">Favorites</Link>
+        <div className="header">
+          <h1>Movies app</h1>
+        </div>
+        <div className="linkSection">
+          <Link className="link" to="/home">
+            Home
+          </Link>
+          <Link className="link" to="/favorites">
+            Favorites
+          </Link>
+        </div>
       </nav>
       <Outlet />
     </>
